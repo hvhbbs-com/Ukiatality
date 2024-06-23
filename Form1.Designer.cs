cc044e7d0195231e8eb121cb1d4f9faec05fd11d
渟雲 <QuQ@outlook.it>
2024-06-24 00:11:12 +0800

    Push

---
+++ Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Ukiatality
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Initialize = new System.Windows.Forms.Button();
+            this.ConfigList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Dir = new System.Windows.Forms.TextBox();
+            this.Load = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Refresh = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CFGName = new System.Windows.Forms.TextBox();
+            this.Create = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Initialize
+            // 
+            this.Initialize.Location = new System.Drawing.Point(297, 7);
+            this.Initialize.Name = "Initialize";
+            this.Initialize.Size = new System.Drawing.Size(75, 23);
+            this.Initialize.TabIndex = 0;
+            this.Initialize.Text = "Initialize";
+            this.Initialize.UseVisualStyleBackColor = true;
+            this.Initialize.Click += new System.EventHandler(this.Initialize_Click);
+            // 
+            // ConfigList
+            // 
+            this.ConfigList.FormattingEnabled = true;
+            this.ConfigList.ItemHeight = 12;
+            this.ConfigList.Location = new System.Drawing.Point(12, 36);
+            this.ConfigList.Name = "ConfigList";
+            this.ConfigList.Size = new System.Drawing.Size(279, 88);
+            this.ConfigList.TabIndex = 1;
+            this.ConfigList.SelectedIndexChanged += new System.EventHandler(this.ConfigList_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ConfigDir:";
+            // 
+            // Dir
+            // 
+            this.Dir.Location = new System.Drawing.Point(83, 9);
+            this.Dir.Name = "Dir";
+            this.Dir.Size = new System.Drawing.Size(208, 21);
+            this.Dir.TabIndex = 3;
+            // 
+            // Load
+            // 
+            this.Load.Enabled = false;
+            this.Load.Location = new System.Drawing.Point(297, 41);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 4;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Save
+            // 
+            this.Save.Enabled = false;
+            this.Save.Location = new System.Drawing.Point(297, 70);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 5;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Refresh
+            // 
+            this.Refresh.Enabled = false;
+            this.Refresh.Location = new System.Drawing.Point(297, 99);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(75, 23);
+            this.Refresh.TabIndex = 6;
+            this.Refresh.Text = "Refresh";
+            this.Refresh.UseVisualStyleBackColor = true;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ConfigName:";
+            // 
+            // CFGName
+            // 
+            this.CFGName.Location = new System.Drawing.Point(90, 128);
+            this.CFGName.Name = "CFGName";
+            this.CFGName.Size = new System.Drawing.Size(201, 21);
+            this.CFGName.TabIndex = 8;
+            // 
+            // Create
+            // 
+            this.Create.Enabled = false;
+            this.Create.Location = new System.Drawing.Point(297, 128);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(75, 23);
+            this.Create.TabIndex = 9;
+            this.Create.Text = "Create";
+            this.Create.UseVisualStyleBackColor = true;
+            this.Create.Click += new System.EventHandler(this.Create_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(359, 36);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "The Israeli occupation of Palestine has caused THOUSANDS OF\r\nCHILDREN to suffer w" +
+    "hile u are enjoy your HvH gaming time. \r\nConsider helping them:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(144, 177);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(29, 12);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "PCRF";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(180, 177);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(41, 12);
+            this.linkLabel2.TabIndex = 12;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "UNICEF";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(228, 177);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(35, 12);
+            this.linkLabel3.TabIndex = 13;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "UNRWA";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(269, 180);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 73);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(239, 60);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "In addition, we hope that you contact \r\npublic opinion representatives in your \r\n" +
+    "area to convey your concerns about \r\nchildren in Palestine and urge your \r\ngover" +
+    "nment to take action for peace.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Create);
+            this.Controls.Add(this.CFGName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Refresh);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Dir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ConfigList);
+            this.Controls.Add(this.Initialize);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "FALITATA Config Loader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Initialize;
+        private System.Windows.Forms.ListBox ConfigList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Dir;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Refresh;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CFGName;
+        private System.Windows.Forms.Button Create;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Management;
+using System.IO;
+
+namespace Ukiatality
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Initialize_Click(object sender, EventArgs e)
+        {
+            // find config dir
+            string myDocumentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string targetFolderPath = Path.Combine(myDocumentsPath, "FALITATA");
+            if (!Directory.Exists(targetFolderPath))
+            {
+                Directory.CreateDirectory(targetFolderPath);
+            }
+            // list all config
+            string[] cfgFiles = Directory.GetFiles(targetFolderPath, "*", SearchOption.AllDirectories);
+            ConfigList.Items.Clear();
+            foreach (string cfgFile in cfgFiles)
+            {
+                string fileName = Path.GetFileName(cfgFile);
+                ConfigList.Items.Add(fileName);
+            }
+            Refresh.Enabled = true;
+            Create.Enabled = true;
+            // find cs dir
+            string processName = "CS2.EXE";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher($"SELECT * FROM Win32_Process WHERE Name = '{processName}'");
+            bool foundProcess = false;
+            foreach (ManagementObject process in searcher.Get())
+            {
+                string processPath = process["ExecutablePath"]?.ToString();
+                if (!string.IsNullOrEmpty(processPath))
+                {
+                    string directoryPath = System.IO.Path.GetDirectoryName(processPath);
+                    Dir.Text = directoryPath;
+                    foundProcess = true;
+                    Load.Enabled = true;
+                    Save.Enabled = true;
+                }
+            }
+            if (!foundProcess)
+            {
+                Dir.Text = "Plz start CS2 first!!";
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure to save?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+
+                string configFile = Path.Combine(Dir.Text, "fatal_settings.cfg");
+                string content = File.ReadAllText(configFile);
+                string ConfigName = (string)ConfigList.SelectedItem;
+                string myDocumentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+                string targetFolderPath = Path.Combine(myDocumentsPath, "FALITATA");
+                string configPath = Path.Combine(targetFolderPath, ConfigName);
+
+                File.WriteAllText(configPath, content);
+
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            // find config dir
+            string myDocumentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string targetFolderPath = Path.Combine(myDocumentsPath, "FALITATA");
+            if (!Directory.Exists(targetFolderPath))
+            {
+                Directory.CreateDirectory(targetFolderPath);
+            }
+            // list all config
+            string[] cfgFiles = Directory.GetFiles(targetFolderPath, "*", SearchOption.AllDirectories);
+            ConfigList.Items.Clear();
+            foreach (string cfgFile in cfgFiles)
+            {
+                string fileName = Path.GetFileName(cfgFile);
+                ConfigList.Items.Add(fileName);
+            }
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure to load?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+
+                string configFile = Path.Combine(Dir.Text, "fatal_settings.cfg");
+                string ConfigName = ConfigList.SelectedItem.ToString();
+                string myDocumentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+                string targetFolderPath = Path.Combine(myDocumentsPath, "FALITATA");
+                string configPath = Path.Combine(targetFolderPath, ConfigName);
+                string fileContent = File.ReadAllText(configPath);
+
+                File.WriteAllText(configFile, fileContent);
+
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Create_Click(object sender, EventArgs e)
+        {
+            string myDocumentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string targetFolderPath = Path.Combine(myDocumentsPath, "FALITATA");
+            string configFile = Path.Combine(targetFolderPath, CFGName.Text);
+            if (!File.Exists(configFile) && CFGName.Text != "Config already exist!!")
+            {
+                // create file
+                FileStream fs = File.Create(configFile);
+                fs.Close(); // close file stream
+            }
+            else
+            {
+                CFGName.Text = "Config already exist!!";
+            }
+        }
+
+        private void ConfigList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ConfigList.SelectedItem != null)
+            {
+                CFGName.Text = ConfigList.SelectedItem.ToString();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.pcrf.net/");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://help.unicef.org/donate-to-children");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://donate.unrwa.org/gaza/~my-donation");
+        }
+    }
+}
